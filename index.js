2bd8b111237919262129f7294d7647752f133701
Fahima <sfahima1388@gmail.com>
2023-09-01 17:25:53 +0600

    added no content found div

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const loadData = async () => {
     // console.log(tab);
     const tabDiv = document.createElement("div");
     tabDiv.innerHTML = `
-    <button
+    <button onclick=changeColor() id="tab-btn"
           class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-colGrey text-btnGrey text-xl font-medium"
         >
-          <a class="tab" onclick="loadVideos('${tab.category_id}')">${tab.category}</a>
+          <a class="tab text-center" onclick="loadVideos('${tab.category_id}')">${tab.category}</a>
     `;
     tabContainer.appendChild(tabDiv);
   });
@@ -24,8 +24,8 @@ const loadVideos = async (categoryId) => {
   );
   const data = await res.json();
   const videos = data.data;
-  console.log(videos);
   const mainContent = document.getElementById("main-content");
+  mainContent.innerHTML = "";
   videos?.forEach((video) => {
     const videoDiv = document.createElement("div");
     videoDiv.innerHTML = `
@@ -42,8 +42,27 @@ const loadVideos = async (categoryId) => {
   </div>
 </div>`;
     mainContent.appendChild(videoDiv);
+    // error content
+    console.log(data?.status);
+    const errContent = document.getElementById("err-content");
+    if (data?.status !== "true") {
+      errContent.innerHTML = `
+  <img src="images/Icon.png" alt="" />
+  <h3 class="text-4xl font-bold text-black">
+    Oops!! Sorry, There is no content here.
+  </h3>`;
+    } else {
+      errContent.innerHTML = "";
+    }
   });
 };
+const changeColor = () => {
+  //button color change
+  document.getElementById("tab-btn").style.backgroundColor = "#FF1F3D";
+  document.getElementById("tab-btn").style.color = "#FFF";
+};
+
 loadData();
-loadVideos("1003");
+loadVideos("1001");
+
 // main content starts here
