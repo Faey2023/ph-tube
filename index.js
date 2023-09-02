86367577c9a938873c8ff5176c88882f5ef5fcae
Fahima <sfahima1388@gmail.com>
2023-09-02 16:30:23 +0600

    added verified badge

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const loadData = async () => {
   );
   const data = await res.json();
   const categories = data.data;
-  // console.log(categories);
+  console.log(categories);
   const tabContainer = document.getElementById("tab-container");
   categories.forEach((tab) => {
     // console.log(tab);
     const tabDiv = document.createElement("div");
     tabDiv.innerHTML = `
-    <button onclick=changeColor() id="tab-btn"
+    <button
           class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-colGrey text-btnGrey text-xl font-medium"
         >
           <a class="tab text-center" onclick="loadVideos('${tab.category_id}')">${tab.category}</a>
@@ -26,42 +26,49 @@ const loadVideos = async (categoryId) => {
   const videos = data.data;
   const mainContent = document.getElementById("main-content");
   mainContent.innerHTML = "";
+  if (videos?.length != 0) {
+    const errDiv = document.getElementById("err-content");
+    errDiv.classList.add("hidden");
+  } else {
+    const errDiv = document.getElementById("err-content");
+    errDiv.classList.remove("hidden");
+  }
   videos?.forEach((video) => {
+    let second = video?.others?.posted_date;
+    let minute = Math.floor(second / 60);
+    let hour = Math.floor(minute / 60);
+    let ultimateMinute = minute - hour * 60;
+    // console.log(hour, ultimateMinute);
+    const time = ` ${hour}hours ${ultimateMinute}minutes ago.`;
+    console.log(time);
     const videoDiv = document.createElement("div");
     videoDiv.innerHTML = `
-    <div>
+    <div class="relative flex flex-col">
   <img class="w-[312px] h-[200px] mb-5" src="${video?.thumbnail}" alt="" />
+  <p class="bg-black text-white text-[10px] font-normal max-w-full absolute mt-40 ml-52">${time}</p>
 </div>
 <div class="flex justify-items-start gap-3">
-  <img class="rounded-full max-w-[40px] max-h-[40px]" src="${video?.authors[0].profile_picture}" alt="" />
+  <img class="rounded-full max-w-[40px] max-h-[40px]" src="${
+    video?.authors[0].profile_picture
+  }" alt="" />
   <div>
     <h1 class="text-black text-[16px] font-bold">${video?.title}</h1>
-    <p class="inline-block text-darkGrey text-[14px] font-normal">${video?.authors[0].profile_name}</p>
-    
+    <div class="flex gap-2">
+    <p class=" text-darkGrey text-[14px] font-normal">${
+      video?.authors[0].profile_name
+    }</p>
+    <img class="max-w-[20px]" src="${
+      video?.authors[0].verified
+        ? "https://upload.wikimedia.org/wikipedia/commons/e/e4/Twitter_Verified_Badge.svg"
+        : ""
+    } "/>
+    </div>
     <p class="text-darkGrey text-[14px] font-normal">${video?.others?.views}</p>
   </div>
 </div>`;
     mainContent.appendChild(videoDiv);
-    // error content
-    console.log(data?.status);
-    const errContent = document.getElementById("err-content");
-    if (data?.status !== "true") {
-      errContent.innerHTML = `
-  <img src="images/Icon.png" alt="" />
-  <h3 class="text-4xl font-bold text-black">
-    Oops!! Sorry, There is no content here.
-  </h3>`;
-    } else {
-      errContent.innerHTML = "";
-    }
   });
 };
-const changeColor = () => {
-  //button color change
-  document.getElementById("tab-btn").style.backgroundColor = "#FF1F3D";
-  document.getElementById("tab-btn").style.color = "#FFF";
-};
-
 loadData();
 loadVideos("1001");
 
